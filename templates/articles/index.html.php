b2b0771066afa93efedadcfc4cd9268f155cec5c
Chmouel Mazouz <chmouelmazouz@gmail.com>
2019-12-30 19:56:55 +0100

    Intégration de Matérialize

--- templates/articles/index.html.php
+++ templates/articles/index.html.php
@@ -1,10 +1,30 @@
-<h1>Nos articles</h1>
+<div id="index-banner" class="parallax-container">
+    <div class="section no-pad-bot">
+        <div class="container">
+            <br><br>
+            <h1 class="header center teal-text text-lighten-2">LE blog de Jean Forteroche</h1>
+            <div class="row center">
+                <h5 class="header col s12 light">Un billet pour l'alaska</h5>
+            </div>
+            <div class="row center">
+                <a href="http://materializecss.com/getting-started.html" id="download-button" class="btn-large waves-effect waves-light teal lighten-1">Get Started</a>
+            </div>
+            <br><br>
 
-<?php foreach ($articles as $article) : ?>
-    <h2><?= $article['title'] ?></h2>
-    <small>Ecrit le <?= $article['created_at'] ?></small>
-    <p><?= $article['introduction'] ?></p>
-    <a href="index.php?controller=articleController&task=show&id=<?= $article['id'] ?>">Lire la suite</a>
-    <a href="index.php?controller=articleController&task=update&id=<?= $article['id'] ?>">Modifier</a>
-    <a href="index.php?controller=articleController&task=delete&id=<?= $article['id'] ?>" onclick="return window.confirm(`Êtes vous sur de vouloir supprimer cet article ?!`)">Supprimer</a>
-<?php endforeach ?>
+        </div>
+    </div>
+    <div class="parallax"><img src="images/background1.jpg" alt="Unsplashed background img 1"></div>
+</div>
+
+<div class="container">
+    <h1>Nos articles</h1>
+
+    <?php foreach ($articles as $article) : ?>
+        <h2><?= $article['title'] ?></h2>
+        <small>Ecrit le <?= $article['created_at'] ?></small>
+        <p><?= $article['introduction'] ?></p>
+        <a href="index.php?controller=articleController&task=show&id=<?= $article['id'] ?>">Lire la suite</a>
+        <a href="index.php?controller=articleController&task=update&id=<?= $article['id'] ?>">Modifier</a>
+        <a href="index.php?controller=articleController&task=delete&id=<?= $article['id'] ?>" onclick="return window.confirm(`Êtes vous sur de vouloir supprimer cet article ?!`)">Supprimer</a>
+    <?php endforeach ?>
+</div>
--- templates/layout.html.php
+++ templates/layout.html.php
@@ -2,48 +2,81 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8"/>
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <title>Blog de Jean Forteroche -<?= $pageTitle ?></title>
+    <!-- CSS  -->
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <link href="css/materialize.css" type="text/css" rel="stylesheet" media="screen,projection"/>
     <link href="css/style.css" type="text/css" rel="stylesheet" media="screen,projection"/>
+    <link href="https://fonts.googleapis.com/css?family=Long+Cang&display=swap" rel="stylesheet">
     <script src="https://cdn.tiny.cloud/1/no-api-key/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
     <script>tinymce.init({selector:'textarea'});</script>
-    <title>Mon superbe blog - <?= $pageTitle ?></title>
 </head>
 
 <body>
-<header>
-    <nav class="grey darken-3" role="navigation">
-        <div class="nav-wrapper container">
-            <a id="logo-container" href="#" class="brand-logo green-text text-darken-3">Blog de Jean Forteroche</a>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="index.php?controller=adminController&task=registerAction" class="green-text text-darken-1">S'inscrire</a></li>
-            </ul>
-            <ul class="right hide-on-med-and-down">
-                <li><a href="index.php?controller=adminController&task=login" class="green-text text-darken-1">Se connecter</a></li>
-            </ul>
-        </div>
-    </nav>
-</header>
-<div class="col s12 m8 offset-m2 l6 offset-l3">
-    <div class="card-panel grey lighten-5 z-depth-1">
-        <div class="row valign-wrapper">
-            <div class="col s2">
-                <img src="images/fond.jpg" alt="" class="circle responsive-img"> <!-- notice the "circle" class -->
+<nav class="white" role="navigation">
+    <div class="nav-wrapper container">
+        <a id="logo-container" href="#" class="brand-logo">Jean Forteroche</a>
+        <ul class="right hide-on-med-and-down">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Billets</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="index.php?controller=adminController&task=login">Connexion</a></li>
+        </ul>
+
+        <ul id="nav-mobile" class="sidenav">
+            <li><a href="#">Accueil</a></li>
+            <li><a href="#">Billets</a></li>
+            <li><a href="#">Contact</a></li>
+            <li><a href="index.php?controller=adminController&task=login">Connexion</a></li>
+        </ul>
+        <a href="#" data-target="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+    </div>
+</nav>
+    <?= $pageContent ?>
+
+<footer class="page-footer teal">
+    <div class="container">
+        <div class="row">
+            <div class="col l6 s12">
+                <h5 class="white-text">Company Bio</h5>
+                <p class="grey-text text-lighten-4">We are a team of college students working on this project like it's our full time job. Any amount would help support and continue development on this project and is greatly appreciated.</p>
+
+
             </div>
-            <div class="col s10">
-              <span class="black-text">
-                This is a square image. Add the "circle" class to it to make it appear circular.
-              </span>
+            <div class="col l3 s12">
+                <h5 class="white-text">Settings</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
+            </div>
+            <div class="col l3 s12">
+                <h5 class="white-text">Connect</h5>
+                <ul>
+                    <li><a class="white-text" href="#!">Link 1</a></li>
+                    <li><a class="white-text" href="#!">Link 2</a></li>
+                    <li><a class="white-text" href="#!">Link 3</a></li>
+                    <li><a class="white-text" href="#!">Link 4</a></li>
+                </ul>
             </div>
         </div>
     </div>
-</div>
+    <div class="footer-copyright">
+        <div class="container">
+            Réalisé par Chmouel Mazouz - Etudiant Openclassrooms
+        </div>
+    </div>
+</footer>
 
-
-
-    <?= $pageContent ?>
+<script src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+<script src="js/materialize.js"></script>
+<script src="js/init.js"></script>
 </body>
+
+
 
 </html>
